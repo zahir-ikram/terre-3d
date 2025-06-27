13468f0ffc7ff176a09be73eab750c1374c48baf
zahir-ikram <zahirikram09@gmail.com>
2025-06-27 16:19:13 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
@@ -6,11 +5,15 @@ import EarthModel from './EarthModel'
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 50], fov: 60 }}>
+    <Canvas
+      camera={{ position: [0, 0, 30], fov: 60 }}
+      gl={{ alpha: true }}
+      style={{ background: 'transparent' }}
+    >
       <ambientLight intensity={0.4} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
       <EarthModel url="terre.stl" />
-      <OrbitControls />
+      <OrbitControls enableZoom={false} enablePan={false} />
     </Canvas>
   )
 }
--- src/EarthModel.jsx
+++ src/EarthModel.jsx
@@ -1,20 +1,15 @@
-// EarthModel.jsx
 import React, { useRef, useEffect } from 'react'
 import { useLoader, useFrame } from '@react-three/fiber'
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
-import * as THREE from 'three'
 
-export default function EarthModel() {
-  // Charge le fichier STL dans public/terre.stl (adapte le chemin si besoin)
-  const geometry = useLoader(STLLoader, '/terre.stl')
+export default function EarthModel({ url }) {
+  const geometry = useLoader(STLLoader, url)
   const meshRef = useRef()
 
-  // Centre la géométrie une fois chargée
   useEffect(() => {
     geometry.center()
   }, [geometry])
 
-  // Rotation continue autour de l’axe Y
   useFrame(() => {
     if (meshRef.current) {
       meshRef.current.rotation.y += 0.005
@@ -25,11 +20,11 @@ export default function EarthModel() {
     <mesh
       ref={meshRef}
       geometry={geometry}
-      scale={[0.05, 0.05, 0.05]}
+      scale={[0.15, 0.15, 0.15]}
       castShadow
       receiveShadow
     >
-      <meshStandardMaterial color="#2e8b57" /> {/* couleur vert Terre */}
+      <meshStandardMaterial color="#aaaaaa" />
     </mesh>
   )
 }
